c5f8eaa9998246e568af34f6babab5dca8d6ae3a
daracoo <dkuzmanoski21@gmail.com>
2025-01-13 22:43:12 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+        <navbar 
+            :pages="pages" 
+            :active-page="activePage" 
+            :use-dark-nav-bar="useDarkNavBar" 
+            :nav-link-click="(index) => activePage = index" 
+            @toggle-navbar-theme="toggleNavbarTheme">
+        </navbar>        
+
+        <page-viewer :page="pages[activePage]"></page-viewer>
+</template>
+
+<script>
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue';
+
+export default {
+    components: {
+        Navbar,
+        PageViewer,
+    },
+  data() {
+              return {
+                  activePage: 0,
+                  useDarkNavBar: false,
+                  pages: [
+                      {
+                          link: { text: 'Home', url: 'index.html' },
+                          pageTitle: 'Home Page',
+                          content: 'This is the home content',
+                      },
+                      {
+                          link: { text: 'About', url: 'about.html' },
+                          pageTitle: 'About Page',
+                          content: 'This is the about content',
+                      },
+                      {
+                          link: { text: 'Contact', url: 'contact.html' },
+                          pageTitle: 'Contact Page',
+                          content: 'This is the contact content',
+                      },
+                  ],
+              };
+          },
+}
+</script>
---
+++ src/components/PageViewer.vue
@@ -0,0 +1,14 @@
+<template>
+                <div class="container mt-4">
+                    <h1>{{ page.pageTitle }}</h1>
+                    <p>{{ page.content }}</p>
+                </div>
+</template>
+
+<script>
+export default 
+    {
+     props: ['page'],
+}
+
+</script>
