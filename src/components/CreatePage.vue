60b849959c59111ac8522b2d97ad0573f4a7c7ce
daracoo <dkuzmanoski21@gmail.com>
2025-01-14 23:33:08 +0100

    validating forms

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -23,9 +23,66 @@
                     v-model="content">
                 </textarea>
             </div>
+            <div class="container mb-3">
+                    <div class="mb-3">
+                        <label for="" class="form-label">
+                            Page Title
+                        </label>
+                        <input 
+                            type="text" 
+                            class="form-control"
+                        />
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">
+                            Content
+                        </label>
+                        <textarea 
+                            type="text" 
+                            class="form-control" 
+                            rows="5">
+                        </textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">
+                            Link Text
+                        </label>
+                        <input 
+                            type="text" 
+                            class="form-control"
+                            v-model="linkText"
+                        />
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">
+                            Link URL
+                        </label>
+                        <input 
+                            type="text" 
+                            class="form-control"
+                            v-model="linkUrl"
+                        />
+                    </div>
+                    <div class="row mb-3">
+                        <div class="form-check">
+                            <input 
+                                class="form-check-input" 
+                                type="checkbox" 
+                                id="gridCheck1"
+                            />
+                            <label 
+                                class="form-check-label" 
+                                for="gridCheck1">
+                                Published
+                            </label>
+                        </div>
+                    </div>
+            </div>
+
             <div class="mb-3">
                 <button class="btn btn-primary"
-                @click.prevent="pageCreated({pageTitle, content})">
+                :disabled="isFromInvalid"
+                @click.prevent="submitForm">
                     
                     Create Page
                 </button>
@@ -37,10 +94,34 @@
 <script>
 export default{
     props: ['pageCreated'],
+    computed:{
+        isFromInvalid(){
+            return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl;
+        }
+    },
     data(){
         return {
             pageTitle: '',
-            content: ''
+            content: '',
+            linkText: '',
+            linkUrl: ''
+        }
+    },
+    methods: {
+        submitForm(){
+            if(!this.pageTitle || !this.content || !this.linkText || !this.linkUrl){
+                alert("Please fill the form.");
+                return;
+            }
+
+            this.pageCreated({
+                pageTitle: this.pageTitle,
+                content: this.content,
+                link: {
+                    text: this.linkText,
+                    link:this.linkText
+                }
+            })
         }
     }
 }</script>
